d81870effae544f42db57f3fc0c6d0f6c810d3e6
Alesia-zuy <alesia20021@mail.ru>
2022-09-08 11:33:08 +0300

    create two blueprint

---
+++ first/main.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+
+view_photo_blueprint = Blueprint('view_photo_blueprint', __name__)
+
---
+++ second/loader.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+
+load_photo_blueprint = Blueprint('load_photo_blueprint', __name__)
+
